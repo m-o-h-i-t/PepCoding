ac4f0fa9cf6085dc924d33d035b91eb35484a0f4
m-o-h-i-t <mohitagrawalone2@gmail.com>
2021-07-22 11:43:47 +0530

    master : adds two nmbers

---
+++ src/GettingStarted/AddNumbers.java
@@ -0,0 +1,12 @@
+package GettingStarted;
+
+public class AddNumbers {
+
+    // Adds two numbers --Hardcoded
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a+b);
+    }
+}
